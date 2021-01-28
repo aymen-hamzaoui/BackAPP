b679930eff974b329f016a3bea854f019a7efb24
Aymen HAMZAOUI <aymenhamzaoui02@gmail.com>
2021-01-28 18:18:50 +0100

    first commit

---
+++ src/main/java/com/rest/demo/business/GreetingBusiness.java
@@ -0,0 +1,18 @@
+package com.rest.demo.business;
+
+import com.rest.demo.bean.Greeting;
+
+public class GreetingBusiness {
+    public Greeting greeting(String message, String auteur) {
+        Greeting grt = new Greeting();
+        grt.setAuteur(auteur);
+        grt.setMessage("Bonjouur" + message + "ravi de vous revoir en bonne forme <3");
+        return grt;
+    }
+
+    public Greeting badGreeting() {
+        Greeting grt = new Greeting();
+        grt.setMessage("Votre nom est ce qui fait de vous une personne si spéciale. N'ayez aucune crainte à le mentionner :). Merci de le renseigner sous la forme gretting/message/nom");
+        return grt;
+    }
+}
